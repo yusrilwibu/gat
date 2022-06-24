d41d85513fd1dcbcb193b856e476f5be2ec86071
yusrilwibu <104910636+yusrilwibu@users.noreply.github.com>
2022-06-24 22:31:01 +0700

    Update config.js

--- config.js
+++ config.js
@@ -13,7 +13,7 @@ global.owner = [
 global.mods = [] // Want some help?
 global.prems = [] // Premium user has unlimited limit
 global.lolkey = 'YourApikey'
-global.xkey = 'YourApikey'
+global.xkey = 'nyyxz-bot'
 global.APIs = { // API Prefix
   // name: 'https://website'
   nrtm: 'https://nurutomo.herokuapp.com',
